6d14f59dad36711bd43c857b406350797ff2c7d3
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-04-16 03:16:26 -0400

    Adding magic method to make filter easier.  Fixes CWGEN-16

--- FilterBuilder.php
+++ FilterBuilder.php
@@ -7,7 +7,9 @@ namespace Spinen\ConnectWise\Client;
  *
  * @package Spinen\ConnectWise\Client
  */
+use DateTime;
 use InvalidArgumentException;
+use RuntimeException;
 
 /**
  * Class FilterBuilder
@@ -44,13 +46,17 @@ class FilterBuilder
      *
      * @var array
      */
+    // NOTE: Order is important as we match top down
+    /**
+     * @var array
+     */
     protected $operators = [
-        '='        => 'Equals',
         '!='       => 'NotEquals',
-        '<'        => 'LessThan',
         '<='       => 'LessThanEquals',
-        '>'        => 'GreaterThan',
         '>='       => 'GreaterThanEquals',
+        '='        => 'Equals',
+        '<'        => 'LessThan',
+        '>'        => 'GreaterThan',
         'contains' => 'Contains',
         'like'     => 'Like',
     ];
@@ -63,6 +69,41 @@ class FilterBuilder
     {
         $this->client = $client;
         $this->filter = $filter;
+    }
+
+    /**
+     * @param $method
+     * @param $arguments
+     *
+     * @return mixed
+     * @throws RuntimeException
+     */
+    function __call($method, $arguments)
+    {
+        $parts = [];
+
+        // Setter like setLimit
+        if (preg_match("/^set([A-Z].+)/u", $method, $parts)) {
+            $property = $parts[1];
+            $value = $arguments[0];
+
+            return call_user_func_array([$this, 'set'], compact("property", "value"));
+        }
+
+        // Conditions builder
+        if (preg_match("/^(and|or)?(?:W|w)here(.+)/u", $method, $parts)) {
+            foreach ($this->operators as $operator => $description) {
+                if (ends_with($parts[2], $description)) {
+                    $property = str_replace($description, "", $parts[2]);
+                    $value = $arguments[0];
+                    $logical = $parts[1];
+
+                    return call_user_func_array([$this, 'where'], compact("property", "operator", "value", "logical"));
+                }
+            }
+        }
+
+        throw new RuntimeException(sprintf("Method [%s] not found.", $method));
     }
 
     /**
@@ -92,10 +133,44 @@ class FilterBuilder
     }
 
     /**
+     * Pads the value with the proper delimiters based on type
+     *
+     * @param mixes $value
+     *
+     * @return string
+     */
+    private function padValue($value)
+    {
+        if ($value instanceof DateTime) {
+            // ISO 8601
+            return '[' . $value->format('c') . ']';
+        }
+
+        return '\'' . $value . '\'';
+    }
+
+    /**
+     * Set a property in the query
+     *
+     * @param string $property
+     * @param mixed  $value
+     *
+     * @return $this
+     */
+    public function set($property, $value)
+    {
+        $this->filter[$property] = $this->padValue($value);
+
+        return $this;
+    }
+
+    /**
+     * Set the orderBys as there can be more than one
+     *
      * @param      $column
      * @param null $order
      */
-    public function orderBy($column, $order = null)
+    public function setOrderBy($column, $order = null)
     {
         if ((!is_null($order)) && (!strcasecmp('desc', $order) === 0)) {
             throw new InvalidArgumentException(sprintf("Invalid order [%s] to order the columns by.", $order));
@@ -115,32 +190,17 @@ class FilterBuilder
     }
 
     /**
-     * Set a property in the query
-     *
-     * @param string $property
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function set($property, $value)
-    {
-        $this->filter[$property] = $value;
-
-        return $this;
-    }
-
-    /**
      * Build a where condition for the filter
      *
      * @param string      $property
      * @param string      $operator
-     * @param string      $value
+     * @param mixed       $value
      * @param string|null $logical
      *
      * @return $this
      * @throws InvalidArgumentException
      */
-    public function where($property, $operator, $value, $logical = 'AND')
+    public function where($property, $operator, $value, $logical = null)
     {
         if (!array_key_exists($operator, $this->operators)) {
             throw new InvalidArgumentException(sprintf("Provided an invalid operator [%s].", $operator));
@@ -148,7 +208,9 @@ class FilterBuilder
 
         $logical = $this->whereConditionPad($logical);
 
-        $this->filter['conditions'] .= $logical . $property . ' ' . $operator . ' ' . $value;
+        $value = $this->padValue($value);
+
+        $this->filter['conditions'] .= implode(" ", [$logical, $property, $operator, $value]);
 
         return $this;
     }
@@ -163,21 +225,22 @@ class FilterBuilder
      */
     private function whereConditionPad($logical)
     {
+        // If no logical or no existing conditions, then set conditions to null
+        if ((empty($logical)) || (!array_key_exists('conditions', $this->filter))) {
+            return $this->filter['conditions'] = null;
+        }
+
+        $logical = strtoupper($logical);
+
         if (!in_array($logical, $this->logicals)) {
             throw new InvalidArgumentException(sprintf("Provided an invalid logical operator [%s].", $logical));
         }
 
-        if (!array_key_exists('conditions', $this->filter)) {
-            $this->filter['conditions'] = null;
-
-            return null;
-        }
-
-        return ' ' . $logical . ' ';
+        return ' ' . $logical;
     }
 
     /**
-     * Raw condition statment
+     * Raw condition statement
      *
      * @param string      $condition
      * @param string|null $logical
@@ -185,7 +248,7 @@ class FilterBuilder
      * @return $this
      * @throws InvalidArgumentException
      */
-    public function whereRaw($condition, $logical = 'AND')
+    public function whereRaw($condition, $logical = null)
     {
         $logical = $this->whereConditionPad($logical);
 
