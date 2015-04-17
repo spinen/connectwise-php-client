2fb457b3393338bd1b750966930dadb1d1b8f05e
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-04-16 21:41:17 -0400

    Building magic methods to make the builder more expressive & cleaning up the file.  CWGEN-16

--- FilterBuilder.php
+++ FilterBuilder.php
@@ -10,7 +10,6 @@ namespace Spinen\ConnectWise\Client;
 use Carbon\Carbon;
 use DateTime;
 use InvalidArgumentException;
-use RuntimeException;
 
 /**
  * Class FilterBuilder
@@ -33,22 +32,10 @@ class FilterBuilder
     protected $filter = [];
 
     /**
-     * The logicals that are supported in the conditions
-     *
-     * @var array
-     */
-    protected $logicals = [
-        'AND',
-        'OR',
-    ];
-
-    /**
      * The operators that is supported in conditions
      *
-     * @var array
-     */
-    // NOTE: Order is important as we match top down
-    /**
+     * NOTE: Order is important as we match top down
+     *
      * @var array
      */
     protected $operators = [
@@ -81,40 +68,38 @@ class FilterBuilder
      * @param $arguments
      *
      * @return mixed
-     * @throws RuntimeException
      */
     function __call($method, $arguments)
     {
         $parts = [];
 
-        // Setter like setLimit
-        if (preg_match("/^set([A-Z].+)/u", $method, $parts)) {
-            $property = $parts[1];
-            $value = $arguments[0];
-
-            return call_user_func_array([$this, 'set'], compact("property", "value"));
+        if ($this->isWhereRawMethod($method, $arguments, $parts)) {
+            return $this->runWhereRawMethod($arguments, $parts);
         }
 
-        // Conditions builder
-        if (preg_match("/^(and|or)?(?:W|w)here(.+)/u", $method, $parts)) {
-            foreach ($this->operators as $operator => $description) {
-                if (ends_with($parts[2], $description)) {
-                    $property = str_replace($description, "", $parts[2]);
-                    $value = $arguments[0];
-                    $logical = $parts[1];
-
-                    return call_user_func_array([$this, 'where'], compact("property", "operator", "value", "logical"));
-                }
-            }
+        if ($this->isWhereWithPropertyMethod($method, $parts)) {
+            return $this->runWhereWithPropertyMethod($arguments, $parts);
         }
 
-        throw new RuntimeException(sprintf("Method [%s] not found.", $method));
+        if ($this->isWhereMethod($method, $parts)) {
+            return $this->runWhereMethod($arguments, $parts);
+        }
+
+        if ($this->isOrderByWithPropertyMethod($method, $parts)) {
+            return $this->runOrderByWithPropertyMethod($parts);
+        }
+
+        if ($this->isOrderByMethod($method, $parts)) {
+            return $this->runOrderByMethod($arguments, $parts);
+        }
+
+        return $this->runSetMethod($method, $arguments);
     }
 
     /**
      * Get the data from the client with the filter
      *
-     * An array of columns canbe passed in to limit the columns that are returned
+     * An array of columns can be passed in to limit the columns that are returned
      *
      * @param array $columns
      *
@@ -126,7 +111,7 @@ class FilterBuilder
             $this->filter['ReturnFields'] = $columns;
         }
 
-        return $this->client->execute((array)$this->filter);
+        return $this->client->execute($this->getFilter());
     }
 
     /**
@@ -144,13 +129,98 @@ class FilterBuilder
      */
     public function getFilter()
     {
-        return $this->filter;
+        return (array)$this->filter;
+    }
+
+    /**
+     * @param string $method
+     * @param array  $parts
+     *
+     * @return bool
+     */
+    private function isOrderByMethod($method, array &$parts)
+    {
+        return (preg_match("/^(and)OrderBy$/u", $method, $parts));
+    }
+
+    /**
+     * @param string $method
+     * @param array  $parts
+     *
+     * @return bool
+     */
+    private function isOrderByWithPropertyMethod($method, array &$parts)
+    {
+        return (preg_match("/^(?:(and)O|o)rderBy(.+?(Asc|Desc)?)$/u", $method, $parts));
+    }
+
+    /**
+     * @param string $method
+     * @param array  $parts
+     *
+     * @return bool
+     */
+    private function isWhereMethod($method, array &$parts)
+    {
+        return (preg_match("/^(and|or)Where$/u", $method, $parts));
+    }
+
+    /**
+     * @param string $method
+     * @param array  $arguments
+     * @param array  $parts
+     *
+     * @return bool
+     */
+    private function isWhereRawMethod($method, array $arguments, array &$parts)
+    {
+        return ((preg_match("/^(and|or)WhereRaw$/u", $method, $parts)) &&
+                (count($arguments) === 1) &&
+                str_contains($arguments[0], array_keys($this->operators)));
+    }
+
+    /**
+     * @param string $method
+     * @param array  $parts
+     *
+     * @return bool
+     */
+    private function isWhereWithPropertyMethod($method, array &$parts)
+    {
+        return (preg_match("/^(?:(?:(and|or)?W)|w)here(.+)$/u", $method, $parts));
+    }
+
+    /**
+     * Set the orderBys as there can be more than one
+     *
+     * @param string      $column
+     * @param string|null $order
+     * @param bool|null   $logical
+     *
+     * @return $this
+     */
+    public function orderBy($column, $order = null, $logical = null)
+    {
+        // Default to asc & make sure that all lower case
+        $order = empty($order) ? 'asc' : strtolower($order);
+
+        if (!in_array($order, ['asc', 'desc'])) {
+            throw new InvalidArgumentException(sprintf("Invalid order [%s] to order the columns by.", $order));
+        }
+
+        $logical = $this->processLogical($logical, 'OrderBy');
+
+        $logical = is_null($logical) ? $logical : ',';
+
+        $this->filter['OrderBy'] .= implode(" ", array_filter(compact("logical", "column", "order")));
+
+        return $this;
     }
 
     /**
      * Pads the value with the proper delimiters based on type
      *
-     * @param mixes $value
+     * @param mixed $value
      *
      * @return string
      */
@@ -158,13 +228,181 @@ class FilterBuilder
     {
         if ($value instanceof DateTime) {
             // Make the value a Carbon if not already so, as DateTime does not return correct ISO8601 date
-            $value = (($value instanceof Carbon) ? $value : Carbon::instance($value));
+            $value = $value instanceof Carbon ? $value : Carbon::instance($value);
 
             // ISO 8601
             return '[' . $value->toIso8601String() . ']';
         }
 
+        if ((is_bool($value)) || (is_integer($value))) {
+            return $value;
+        }
+
+        if (is_null($value)) {
+            return "null";
+        }
+
         return '\'' . $value . '\'';
+    }
+
+    /**
+     * If there are not any existing conditions, then null out the logical.  Otherwise, pad it
+     *
+     * @param string $logical
+     *
+     * @param string $key
+     *
+     * @return null|string
+     */
+    private function processLogical($logical, $key)
+    {
+        // If no logical or no existing conditions, then set logical & conditions to null
+        if ((empty($logical)) || (!array_key_exists($key, $this->filter))) {
+            return $this->filter[$key] = null;
+        }
+
+        $logical = strtoupper($logical);
+
+        if (!in_array($logical, ['AND', 'OR'])) {
+            throw new InvalidArgumentException(sprintf("Provided an invalid logical operator [%s].", $logical));
+        }
+
+        return ' ' . $logical;
+    }
+
+    /**
+     * @param array $arguments
+     * @param array &$parts
+     *
+     * @return $this
+     * @throws InvalidArgumentException
+     */
+    private function runOrderByMethod(array $arguments, array &$parts)
+    {
+        if (!((count($arguments) === 1) || (count($arguments) === 2))) {
+            throw new InvalidArgumentException("You must pass at least a column to order the results.");
+        }
+
+        $parts = array_pad($parts, 2, null);
+
+        $column = $arguments[0];
+        $order = $arguments[1];
+        $continue = $parts[1];
+
+        return $this->orderBy($column, $order, $continue);
+    }
+
+    /**
+     * @param array &$parts
+     *
+     * @return $this
+     */
+    private function runOrderByWithPropertyMethod(array &$parts)
+    {
+        if (!((count($parts) === 3) || (count($parts) === 4))) {
+            throw new InvalidArgumentException("You must pass at least the column and value to filter the results.");
+        }
+
+        $parts = array_pad($parts, 4, null);
+
+        // Trim any Asc or Desc off of the end
+        $column = preg_replace("/^(.*)(?:Asc|Desc)$/u", "$1", $parts[2]);
+        $order = $parts[3];
+        $continue = $parts[1];
+
+        return $this->orderBy($column, $order, $continue);
+    }
+
+    /**
+     * @param $method
+     * @param $arguments
+     *
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
+    private function runSetMethod($method, $arguments)
+    {
+        $property = studly_case($method);
+
+        if (count($arguments) !== 1) {
+            throw new InvalidArgumentException(sprintf("You must pass a value to set for the property [%s].",
+                $property));
+        }
+
+        $value = $arguments[0];
+
+        return $this->set($property, $value);
+    }
+
+    /**
+     * @param array $arguments
+     * @param array &$parts
+     *
+     * @return $this
+     * @throws InvalidArgumentException
+     */
+    private function runWhereMethod(array $arguments, array &$parts)
+    {
+        if (!((count($arguments) === 2) || (count($arguments) === 3))) {
+            throw new InvalidArgumentException("You must pass at least property and value to filter the results.");
+        }
+
+        $parts = array_pad($parts, 2, null);
+
+        $property = $arguments[0];
+        $value = $arguments[1];
+        $operator = $arguments[2];
+        $logical = $parts[1];
+
+        return $this->where($property, $value, $operator, $logical);
+    }
+
+    /**
+     * @param array $arguments
+     * @param array &$parts
+     *
+     * @return $this
+     * @throws InvalidArgumentException
+     */
+    private function runWhereRawMethod(array $arguments, array &$parts)
+    {
+        if (count($arguments) !== 1) {
+            throw new InvalidArgumentException("You must pass a raw query string to filter the results.");
+        }
+
+        $condition = $arguments[0];
+        $logical = $parts[1];
+
+        return $this->whereRaw($condition, $logical);
+    }
+
+    /**
+     * @param array $arguments
+     * @param array &$parts
+     *
+     * @return $this
+     * @throws InvalidArgumentException
+     */
+    private function runWhereWithPropertyMethod(array $arguments, array &$parts)
+    {
+        if (count($arguments) !== 1) {
+            throw new InvalidArgumentException("You must pass a value for the property to filter the results.");
+        }
+
+        $parts = array_pad($parts, 3, null);
+
+        $property = $parts[2];
+        $value = $arguments[0];
+        $logical = $parts[1];
+
+        foreach ($this->operators as $operator => $description) {
+            if (ends_with($property, $description)) {
+                return $this->where(str_replace($description, '', $property), $value, $operator, $logical);
+            }
+        }
+
+        // Default to equals
+        return $this->where($property, $value, '=', $logical);
     }
 
     /**
@@ -183,78 +421,32 @@ class FilterBuilder
     }
 
     /**
-     * Set the orderBys as there can be more than one
-     *
-     * @param      $column
-     * @param null $order
-     */
-    public function setOrderBy($column, $order = null)
-    {
-        if ((!is_null($order)) && (!strcasecmp('desc', $order) === 0)) {
-            throw new InvalidArgumentException(sprintf("Invalid order [%s] to order the columns by.", $order));
-        }
-
-        if (!is_null($order)) {
-            $order = ' ' . $order;
-        }
-
-        $existing = null;
-
-        if (array_key_exists('OrderBy', $this->filter)) {
-            $existing = $this->filter['Orderby'] . ', ';
-        }
-
-        $this->filter['OrderBy'] = $existing . $column . $order;
-    }
-
-    /**
      * Build a where condition for the filter
      *
      * @param string      $property
-     * @param string      $operator
      * @param mixed       $value
+     * @param string|null $operator
      * @param string|null $logical
      *
      * @return $this
      * @throws InvalidArgumentException
      */
-    public function where($property, $operator, $value, $logical = null)
+    public function where($property, $value, $operator = null, $logical = null)
     {
+        // Default to Equals
+        $operator = empty($operator) ? '=' : $operator;
+
         if (!array_key_exists($operator, $this->operators)) {
             throw new InvalidArgumentException(sprintf("Provided an invalid operator [%s].", $operator));
         }
 
-        $logical = $this->whereConditionPad($logical);
+        $logical = $this->processLogical($logical, 'Conditions');
 
         $value = $this->padValue($value);
 
-        $this->filter['conditions'] .= implode(" ", [$logical, $property, $operator, $value]);
+        $this->filter['Conditions'] .= implode(" ", array_filter(compact("logical", "property", "operator", "value")));
 
         return $this;
-    }
-
-    /**
-     * If there are not any existing conditions, then null out the logical.  Otherwise, pad it
-     *
-     * @param string $logical
-     *
-     * @return null|string
-     * @throws InvalidArgumentException
-     */
-    private function whereConditionPad($logical)
-    {
-        // If no logical or no existing conditions, then set conditions to null
-        if ((empty($logical)) || (!array_key_exists('conditions', $this->filter))) {
-            return $this->filter['conditions'] = null;
-        }
-
-        $logical = strtoupper($logical);
-
-        if (!in_array($logical, $this->logicals)) {
-            throw new InvalidArgumentException(sprintf("Provided an invalid logical operator [%s].", $logical));
-        }
-
-        return ' ' . $logical;
     }
 
     /**
@@ -268,9 +460,9 @@ class FilterBuilder
      */
     public function whereRaw($condition, $logical = null)
     {
-        $logical = $this->whereConditionPad($logical);
+        $logical = $this->processLogical($logical, 'Conditions');
 
-        $this->filter['conditions'] .= $logical . $condition;
+        $this->filter['Conditions'] .= $logical . $condition;
 
         return $this;
     }
