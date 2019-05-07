0d0b14aa4565294e0c6fab9250001831d99916af
Jimmy Puckett <jimmy.puckett@spinen.com>
2019-05-07 19:17:11 -0400

    Guard against length

---
+++ src/Exceptions/MalformedRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Spinen\ConnectWise\Exceptions;
+
+use Exception;
+
+/**
+ * Class MalformedRequest
+ *
+ * @package Spinen\ConnectWise\Exceptions
+ */
+class MalformedRequest extends Exception
+{
+}
