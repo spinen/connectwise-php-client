b7358e83591196ecdf1ba29230bbbff20318e50a
Jimmy Puckett <jimmy.puckett@spinen.com>
2019-05-07 14:35:52 -0400

    Allow setting a version in the laravel config

--- src/Laravel/ServiceProvider.php
+++ src/Laravel/ServiceProvider.php
@@ -84,7 +84,8 @@ class ServiceProvider extends LaravelServiceProvider
                     $app->make(ModelResolver::class)
                 ))->setIntegrator($app->config->get('services.connectwise.integrator'))
                   ->setPassword($app->config->get('services.connectwise.password'))
-                  ->setUrl($app->config->get('services.connectwise.url'));
+                  ->setUrl($app->config->get('services.connectwise.url'))
+                  ->setVersion($app->config->get('services.connectwise.version'));
             }
         );
     }
